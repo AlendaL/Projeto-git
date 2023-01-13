b9bf0126211d41b704019a90d362597c52616fac
unknown <alexandreperusio1234567@gmail.com>
2023-01-13 19:59:34 -0300

    testando e compartilhando meus 'estudos'

---
+++ leaning.py
@@ -0,0 +1,11 @@
+wheight = int(input("wheight: "))
+
+unit = input("(L)bs (K)bs: ")
+
+if unit.upper() == "L":
+    converted = wheight * 0.45   
+    print(f"you are {converted} kilograms")
+else: 
+    converted = wheight / 0.45
+    print(f"you are {converted} pounds")
+
