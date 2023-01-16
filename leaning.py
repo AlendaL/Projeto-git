6bde7fb385bb97f0b62f6dc9eec0cfe4ce0d7d96
unknown <alexandreperusio1234567@gmail.com>
2023-01-16 12:59:47 -0300

    Jogo simples de adivinhação

--- leaning.py
+++ leaning.py
@@ -3,17 +3,21 @@ secret = 5
 
 guess_count = 0
 guess_limit = 3
+
 while guess_count < guess_limit:
-    guess = int(input('guess: '))
+    guess = int(input('guess between 0 and 9: '))
     guess_count += 1
+    if guess < 0 or guess > 9:
+        print("Invalid input dotplease enter a number between 0 and 9")
+        continue
     if guess == secret:
         print("yheaaa")
+        break
     elif guess < secret :
         print("its bigger")
     elif guess > secret:
         print("its less")
-    elif guess > guess_limit or guess < guess_count: 
-        print("you need to put and coherent number")
+   
 
 
 # i = 1
