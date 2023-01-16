862ebe20bbf91f829b6af3ccef29bc70266bfd50
unknown <alexandreperusio1234567@gmail.com>
2023-01-16 12:47:12 -0300

    procurando entender com ofazer um jogo de adivinhação

--- leaning.py
+++ leaning.py
@@ -1,10 +1,27 @@
 
-i = 1
+secret = 5
 
-while i <= 100:
-    print("*" * i)
-    i += 1
-print("done")
+guess_count = 0
+guess_limit = 3
+while guess_count < guess_limit:
+    guess = int(input('guess: '))
+    guess_count += 1
+    if guess == secret:
+        print("yheaaa")
+    elif guess < secret :
+        print("its bigger")
+    elif guess > secret:
+        print("its less")
+    elif guess > guess_limit or guess < guess_count: 
+        print("you need to put and coherent number")
+
+
+# i = 1
+
+# while i <= 100:
+#     print("*" * i)
+#     i += 1
+# print("done")
 
 
 # wheight = int(input("wheight: "))
