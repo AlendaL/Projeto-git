05d0bb9f16159be6d52839d162f09ef7c402f5a8
unknown <alexandreperusio1234567@gmail.com>
2023-01-16 17:09:53 -0300

    why a boolean variable called outside of a loop changes it's value when inside a loop like While?

--- leaning.py
+++ leaning.py
@@ -1,22 +1,67 @@
+# why this code changes the started to True after putting the same command?
 
-secret = 5
-
-guess_count = 0
-guess_limit = 3
-
-while guess_count < guess_limit:
-    guess = int(input('guess between 0 and 9: '))
-    guess_count += 1
-    if guess < 0 or guess > 9:
-        print("Invalid input dotplease enter a number between 0 and 9")
-        continue
-    if guess == secret:
-        print("yheaaa")
+started = False
+command = ""
+while command != "quit":
+    command = input("> ").lower()
+    if command == "start":
+        if started:
+            print("car started already")
+        else:
+            started = True
+            print("car started")
+    elif command == "stop":
+        if not started:
+            print("car stopped already")
+        else:
+            started = False
+            print("car stopped.")
+    elif command == "help":
+        print('''
+        Start - To start the car
+        stop - to stop the car 
+        quit - to quit
+        ''')
+    elif command == "quit":
         break
-    elif guess < secret :
-        print("its bigger")
-    elif guess > secret:
-        print("its less")
+    else:
+        print("sorry I don't understand that")
+
+
+# it was made by chatgpt
+# while True:
+#     command = input("Ask for help: ")
+#     if command == "help":
+#         print("start, stop, quit")
+#     elif command == "start":
+#         print("car started")
+#     elif command == "stop":
+#         print("car stopped")
+#     elif command == "quit":
+#         print("game quit")
+#         break
+#     else:
+#         print("Invalid command. Please enter a valid command.")
+
+
+# secret = 5
+
+# guess_count = 0
+# guess_limit = 3
+
+# while guess_count < guess_limit:
+#     guess = int(input('guess between 0 and 9: '))
+#     guess_count += 1
+#     if guess < 0 or guess > 9:
+#         print("Invalid input dotplease enter a number between 0 and 9")
+#         continue
+#     if guess == secret:
+#         print("yheaaa")
+#         break
+#     elif guess < secret :
+#         print("its bigger")
+#     elif guess > secret:
+#         print("its less")
    
 
 
